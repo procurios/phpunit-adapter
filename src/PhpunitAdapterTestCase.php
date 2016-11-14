72feba3f5af2cae560847e2be9e22b1f2f342514
Taco van den Broek <taco.vandenbroek@procurios.nl>
2016-11-14 17:06:39 +0100

    Clean up PhpunitAdapterTestCase class

--- src/PhpunitAdapterTestCase.php
+++ src/PhpunitAdapterTestCase.php
@@ -4,23 +4,29 @@
  */
 namespace Procurios\TDD\PhpunitAdapter;
 
-use PHPUnit_Framework_TestCase;
 use ReflectionClass;
 
-$baseClass = 'Procurios\TDD\PhpunitAdapter\PhpunitAdapterTestCase_prior_5_2';
-
+// Detect the version of PHPUnit we are running on by using class reflection
 $reflectionClass = new ReflectionClass('PHPUnit_Framework_TestCase');
 if ($reflectionClass->hasMethod('expectException')) {
     $baseClass = 'Procurios\TDD\PhpunitAdapter\PhpunitAdapterTestCase_5_2';
 }
 
-// Runtime definition of the PhpunitAdapter class
-$classDefinition = 'namespace Procurios\TDD\PhpunitAdapter;class PhpunitAdapterTestCase extends \\' . $baseClass . ' {}';
-eval($classDefinition);
+if (isset($baseClass)) {
+    // Runtime definition of the PhpunitAdapterTestCase class
+    $classDefinition = <<< PHP
+namespace Procurios\TDD\PhpunitAdapter;
+class PhpunitAdapterTestCase extends \\$baseClass
+{
+}
+PHP;
 
-if (false) {
+    eval($classDefinition);
+}
+
+if (!class_exists('Procurios\TDD\PhpunitAdapter\PhpunitAdapterTestCase', false)) {
     /**
-     * Dummy class providing code completion in IDE's
+     * Fall back on extending the most complete compatibility layer
      */
     class PhpunitAdapterTestCase extends PhpunitAdapterTestCase_prior_5_2
     {
