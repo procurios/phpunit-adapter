dc6d4bc644e8023f9f2c5c4e0695cb731370f0c1
Taco van den Broek <taco.vandenbroek@procurios.nl>
2017-01-23 17:07:28 +0100

    Remove obsolete interface

--- src/PhpunitAdapterTestCase_5_3_0.php
+++ src/PhpunitAdapterTestCase_5_3_0.php
@@ -9,6 +9,6 @@ use PHPUnit_Framework_TestCase;
 /**
  * Compatibility layer for running phpunit 5.3.0
  */
-abstract class PhpunitAdapterTestCase_5_3_0 extends PHPUnit_Framework_TestCase implements PhpunitAdapterInterface
+abstract class PhpunitAdapterTestCase_5_3_0 extends PHPUnit_Framework_TestCase
 {
 }
