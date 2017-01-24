aaeee498da348578aab3396de2a23b771f77e14f
Taco van den Broek <taco.vandenbroek@procurios.nl>
2017-01-24 09:33:40 +0100

    Add namespaced TestCase class for PHPUnit < 5.4

--- src/PhpunitAdapterTestCase_5_3_0.php
+++ src/PhpunitAdapterTestCase_5_3_0.php
@@ -2,31 +2,47 @@
 /**
  * © 2017 Procurios
  */
-namespace Procurios\TDD\PhpunitAdapter;
+namespace PHPUnit\Framework {
 
-use PHPUnit_Framework_MockObject_RuntimeException;
+    use PHPUnit_Framework_TestCase;
 
-/**
- * Compatibility layer for running phpunit 5.3.0
- */
-abstract class PhpunitAdapterTestCase_5_3_0 extends PhpunitAdapterTestCase_5_4_0
-{
-    protected function createMock($className)
-    {
-        if (!class_exists($className, true) && !interface_exists($className, true)) {
-            throw new PHPUnit_Framework_MockObject_RuntimeException(
-                sprintf(
-                    'Cannot stub or mock class or interface "%s" which does not exist',
-                    $className
-                )
-            );
+    if (!class_exists('PHPUnit\Framework\TestCase', true)) {
+        /**
+         * Add namespaced TestCase alias as introduced in PHPUnit 5.4.0
+         */
+        class TestCase extends PHPUnit_Framework_TestCase
+        {
         }
-
-        return $this->getMockBuilder($className)
-            ->disableOriginalConstructor()
-            ->disableOriginalClone()
-            ->disableArgumentCloning()
-            ->getMock()
-        ;
     }
+}
+
+namespace Procurios\TDD\PhpunitAdapter {
+
+    use PHPUnit_Framework_MockObject_RuntimeException;
+
+    /**
+     * Compatibility layer for running phpunit 5.3.0
+     */
+    abstract class PhpunitAdapterTestCase_5_3_0 extends PhpunitAdapterTestCase_5_4_0
+    {
+        protected function createMock($className)
+        {
+            if (!class_exists($className, true) && !interface_exists($className, true)) {
+                throw new PHPUnit_Framework_MockObject_RuntimeException(
+                    sprintf(
+                        'Cannot stub or mock class or interface "%s" which does not exist',
+                        $className
+                    )
+                );
+            }
+
+            return $this->getMockBuilder($className)
+                ->disableOriginalConstructor()
+                ->disableOriginalClone()
+                ->disableArgumentCloning()
+                ->getMock()
+                ;
+        }
+    }
+
 }
